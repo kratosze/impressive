0fb66fe68d9af98abfcd6d910fd4e59dbdb775d0
kratosze <kc006naven@gmail.com>
2024-08-15 20:16:12 +0530

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -87,3 +87,43 @@ papers.forEach(paper => {
   const p = new Paper();
   p.init(paper);
 });
+
+//Changing gif while hovering on a button
+const img = document.getElementById('image');
+
+const images = {
+    love: "images/love.gif",
+    depressed: 'images/depressed.gif',
+    ori: "images/handsome.gif"
+};
+
+function ChangeImage(giphy) {
+    img.src = images[giphy];
+}
+function ResetImage() {
+    img.src = images.ori;
+};
+
+
+//BG image change while hovering on a button
+var body = document.body;
+
+function bg1() {
+	body.className = 'yess';
+};
+
+function bg2() {
+	body.className = 'noo';
+};
+
+function resetBG(){
+	body.className = '';
+};
+
+//Linking to Yes and page
+function yesFunction() {
+    window.location.href = "Page 5/yes.html";
+}
+function noFunction() {
+    window.location.href = "Page 6/no1.html";
+}
